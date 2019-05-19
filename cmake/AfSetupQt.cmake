dd66474621ffa9a7645850057837638f03fcdfd6
Andy Fillebrown <andy.fillebrown@gmail.com>
2019-05-18 20:34:54 -0400

    Link Qt libraries specified in af_setup_qt to given target

--- cmake/AfSetupQt.cmake
+++ cmake/AfSetupQt.cmake
@@ -15,12 +15,15 @@ function(af_setup_qt)
 
 	# Generate list of Qt components from LINK_LIBRARIES argument.
 	set(qt_components "")
+	set(qt_link_libraries "")
 	foreach(link_library ${ARG_LINK_LIBRARIES})
 		# If link_library matches "^Qt.*::.+$" set CMAKE_MATCH_1 to the regex part in parenthesis.
 		if(link_library MATCHES "^Qt.*::(.+)$")
 			list(APPEND qt_components ${CMAKE_MATCH_1})
+			list(APPEND qt_link_libraries Qt5::${CMAKE_MATCH_1})
 		endif()
 	endforeach()
 
 	find_package(Qt5 REQUIRED COMPONENTS ${qt_components})
+	target_link_libraries(${ARG_TARGET} ${qt_link_libraries})
 endfunction()
